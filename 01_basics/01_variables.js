d27ad8be071ed7931391383da6bb8e1cf4277a58
manthan2023 <126338275+manthan2023@users.noreply.github.com>
2024-09-11 05:48:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1452
+let accountName = "Marv"
+var accountPassword = "123"
+accountCity = "Rajkot"
+let accountState
+
+// const can not be change
+
+console.table([accountId,accountName,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+becasuse of issue in block scope and functional scope
+*/
